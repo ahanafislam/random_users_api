df8fb7371e8356f8a792632a84702328ddc2d81d
ahanafislam <ahanafislam@gmail.com>
2022-09-04 20:02:46 +0600

    Make some change

--- middleware/validateUserExistence.js
+++ middleware/validateUserExistence.js
@@ -3,18 +3,26 @@ const { getUsersData } = require("../utils/usersData");
 // For checking the user is exist or not
 const validateUserExistence = (req, res, next) => {
     const users = getUsersData();
-    const {id} = req.body;
+    try {
+        const {id} = req.body;
 
-    const user = users.find(user => user.id === id);
-
-    if(!user) {
+        const user = users.find(user => user.id === id);
+    
+        if(!user) {
+            return res.status(400).send({
+                success: false,
+                messages: `Sorry ${id} is not a valid user id.`,
+            })
+        }
+    
+        next();
+    }
+    catch (e){
         return res.status(400).send({
             success: false,
             messages: `Sorry ${id} is not a valid user id.`,
         })
     }
-
-    next();
 }
 
 module.exports = validateUserExistence;
